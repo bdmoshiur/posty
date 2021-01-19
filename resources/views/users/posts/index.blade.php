5872d3dfd9abd899c2e30ba0b03ef65f134e24f5
Moshiur Rahman <moshiurcse888@gmail.com>
2021-01-19 21:12:05 +0600

    userpost component posts

--- resources/views/users/posts/index.blade.php
+++ resources/views/users/posts/index.blade.php
@@ -1,46 +1,21 @@
 @extends('layouts.app')
 @section('content')
     <div class="flex justify-center">
-        <div class="w-8/12 bg-white p-6 rounded-lg">
-            @if ($posts->count())
-                @foreach ($posts as $post)
-                    <div class="mb-4">
-                        <a href="{{ route('users.posts',$post->user) }}" class="font-bold">{{ $post->user->name }}</a> 
-                        <span class=" text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
-                        <p class="mb-2">{{ $post->body }}</p>
-
-                        @can('delete', $post)
-                        <form action="{{ route('posts.destroy',$post) }}" method="POST">
-                            @csrf
-                            @method("DELETE")
-                            <button type="submit" class="text-blue-500">Delete</button>
-                        </form>
-                        @endcan
-                        <div class="flex items-center">
-                            @auth
-                            @if (!$post->likedBy(auth()->user()))
-                                <form action="{{ route('posts.likes',$post) }}" method="POST" class="mr-1">
-                                    @csrf
-                                    <button type="submit" class="text-blue-500">Like</button>
-                                </form>
-                            @else
-                                <form action="{{ route('posts.likes',$post) }}" method="POST" class="mr-1">
-                                    @csrf
-                                    @method("DELETE")
-                                    <button type="submit" class="text-blue-500">Unlike</button>
-                                </form>
-                            @endif
-                            @endauth
-
-                           <span> {{ $post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
-
-                        </div>
-                    </div>
-                @endforeach
-                {{ $posts->links() }}
-            @else
-                <p>There are no Post!</p>
-            @endif
+        <div class="w-8/12">
+            <div class="p-6">
+                <h2 class="tex-2xl font-medium mb-1">{{ $user->name }}</h2>
+                <p>Posted {{ $posts->count() }} {{ Str::plural('post', $posts->count()) }}</p>
+            </div>
+            <div class="bg-white p-6 rounded-lg">
+                @if ($posts->count())
+                    @foreach ($posts as $post)
+                        <x-post :post="$post" />
+                    @endforeach
+                    {{ $posts->links() }}
+                @else
+                    <p>{{ $user->name }} Does't Have Any Post!</p>
+                @endif
+            </div>
         </div>
     </div>
 @endsection
